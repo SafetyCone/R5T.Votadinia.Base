c42bca7b13278b36b36c24d88f5289a6f1e07479
MinexAutomation <minex.automation@gmail.com>
2019-12-02 16:49:34 -0800

    Added the ICurrentUtcDateTimeProvider service definition interface.

--- source/R5T.Votadinia.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Votadinia.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Votadinia.Base/Code/Services/Interfaces/ICurrentUtcDateTimeProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Votadinia
+{
+    /// <summary>
+    /// Allows service-based indirection for providing the current UTC date-time.
+    /// </summary>
+    public interface ICurrentUtcDateTimeProvider
+    {
+        DateTime GetCurrentUtcDateTime();
+    }
+}
