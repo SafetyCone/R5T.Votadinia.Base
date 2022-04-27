9bfdd7575278cc6d035a105c67403c0007e3918f
davidcoats <david@getsafetycone.com>
2022-04-27 15:37:41 -0700

    Interim changes.

---
+++ source/R5T.Votadinia.Base/Code/Services/Definitions/ICurrentUtcDateTimeProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Votadinia
+{
+    /// <summary>
+    /// Allows service-based indirection for providing the current UTC date-time.
+    /// </summary>
+    [ServiceDefinitionMarker]
+    public interface ICurrentUtcDateTimeProvider : IServiceDefinition
+    {
+        DateTime GetCurrentUtcDateTime();
+    }
+}
